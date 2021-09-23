85eb1b8687729d4f0a547d5a126345e600485a97
Katie Beattie <mkbeattie@loyola.edu>
2021-09-23 06:33:37 -0400

    i created my file!

--- .github/pa1.py
+++ .github/pa1.py
@@ -18,8 +18,8 @@ heightInt = int(heightInput)
 # Calculated area of the room (not including floor
 area = ((2 * (lengthInt * heightInt)) + (2 * (widthInt * heightInt)) + (lengthInt * widthInt))
 # Sq. ft divided by values for paint and primer
-primer = area / 200
-paint = area / 350
+primer = (area // 200) + 1
+paint = (area // 350) + 1
 # Outputs area, gallons of primer and paint
 print("The area of your room is ", area, " square feet.")
 print("You will need ", primer, " gallons of primer to paint the walls in your room.")
